e09aa32f4fc05c5a4f63d8a88f12c3779366fc93
Will <doggy.huang@gmail.com>
2022-11-17 19:12:08 +0800

    Add project files.

---
+++ ProcessManager/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+var data = from p in Process.GetProcesses()
+            where p.MainWindowHandle > 0 && p.ProcessName == "msedge"
+           select p;
+
+foreach (Process edge in data)
+{
+    GracefullyShutdownProcess(edge);
+    
+    Console.WriteLine(edge.ProcessName + " closed.");
+}
+
+void GracefullyShutdownProcess(Process p, int waitSeconds = 9)
+{
+    if (!p.HasExited)
+    {
+        p.CloseMainWindow();
+
+        if (waitSeconds == 0)
+        {
+            p.Kill();
+            return;
+        }
+
+        Console.WriteLine($"Closing {p.ProcessName} process. Counting down: {waitSeconds}");
+
+        Thread.Sleep(1_000);
+
+        p.Refresh();
+        GracefullyShutdownProcess(p, waitSeconds - 1);
+    }
+}
